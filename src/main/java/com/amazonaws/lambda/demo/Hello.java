25d4226303b5f3ba4abb9c7be2a778192523de9d
Kannan <kannan.kct08@gmail.com>
2020-08-15 20:59:40 +0530

    code change

--- src/main/java/com/amazonaws/lambda/demo/Hello.java
+++ src/main/java/com/amazonaws/lambda/demo/Hello.java
@@ -1,9 +1,7 @@
 package com.amazonaws.lambda.demo;
 
-import java.io.File;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.Map;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
@@ -12,41 +10,39 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 
 public class Hello implements RequestHandler<Object, String> {
-	
-	 Regions clientRegion = Regions.US_EAST_1;
-     String bucketName = "store-events-from-api";
-     String stringObjKeyName = "";
-     String fileObjKeyName = "test";
-     String fileName = "input/";
 
-    @Override
-    public String handleRequest(Object input, Context context) {
-    	 context.getLogger().log("Input: " + input);
-    	  stringObjKeyName = ""+input;
-    	  LocalDateTime date = LocalDateTime.now();
-    	  fileName= fileName+date.getYear()+"-"+date.getMonthValue()+"-"+date.getDayOfMonth()+"-"+date.getHour()+"-"+context.getAwsRequestId()+".txt";
-    	  try {
-    	  AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                  .withRegion(clientRegion)
-                  .build();
+	Regions clientRegion = Regions.US_EAST_1;
+	String bucketName = "store-events-from-api";
+	String stringObjKeyName = "";
+	String fileName = "input/";
 
-          s3Client.putObject(bucketName,fileName, stringObjKeyName).getMetadata().setContentType("plain/text");
+	@Override
+	public String handleRequest(Object  input, Context context) {
+		context.getLogger().log("Input: " + input.toString());
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(clientRegion).build();
+		/*
+		 * if(input.getHttpMethod().equals("POST")) { stringObjKeyName = ""+ input; }
+		 * else if(input.getHttpMethod().equals("GET")) { stringObjKeyName = ""+
+		 * input.getQueryStringParameters(); }else {
+		 */
+			stringObjKeyName =""+input;
+		//}
 
+		LocalDateTime date = LocalDateTime.now();
+		String s3fileName = fileName + date.getYear() + "-" + date.getMonthValue() + "-" + date.getDayOfMonth() + "-"
+				+ date.getHour() + "-" + context.getAwsRequestId() + ".txt";
+		try {
+			s3Client.putObject(bucketName, s3fileName, stringObjKeyName).getMetadata().setContentType("plain/text");
 
-      } catch (AmazonServiceException e) {
-          // The call was transmitted successfully, but Amazon S3 couldn't process 
-          // it, so it returned an error response.
-          e.printStackTrace();
-      } catch (SdkClientException e1) {
-          // Amazon S3 couldn't be contacted for a response, or the client
-          // couldn't parse the response from Amazon S3.
-          e1.printStackTrace();
-      }
-      return input.toString();
-    }
+		} catch (AmazonServiceException e) {
+			e.printStackTrace();
+		} catch (SdkClientException e1) {
+
+			e1.printStackTrace();
+		}
+		return input.toString();
+	}
 
 }
